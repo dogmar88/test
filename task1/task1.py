24d88b8a625fe9a3276bc9f494ebf73699debef7
dogmar88 <59418518+dogmar88@users.noreply.github.com>
2020-01-23 22:32:08 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,11 @@
 import sys
-import numpy
+import numpy # well i guess numpy is a basic library for python programmers.
 
 inp=open(sys.argv[1],'r')
 a=[]
 for i in inp:
     a.append(int(i.strip()))
-print (f"{numpy.percentile(a, 90):.{2}f}")
+print (f"{numpy.percentile(a, 90):.{2}f}") # it is decimal formation of numbers after the point. It can be failed in Python 2.
 print (f"{numpy.median(a):.{2}f}")
 print (f"{max(a):.{2}f}")
 print (f"{min(a):.{2}f}")
