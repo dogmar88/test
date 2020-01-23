f10ec388464dfcc29baea3f061c80784995f30fc
dogmar88 <59418518+dogmar88@users.noreply.github.com>
2020-01-23 22:18:26 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,13 +1,13 @@
 import sys
-from shapely.geometry import Polygon, Point
+from shapely.geometry import Polygon, Point # it will not work without this library. I hope you have it installed. Also I wrote a different version, long, but more autonomic - look at the second file.
 
 i1=open(sys.argv[1],'r')
 figura=[]
 for i in i1:
-   figura.append(list(map(float, i.split())))
+   figura.append(list(map(float, i.split()))) # creating a figure for polygon class
 poly = Polygon([(figura[0]),(figura[1]),(figura[2]),(figura[3])])
 i2=open(sys.argv[2],'r')
-for i in i2:
+for i in i2: # applying methods from geometry
     p=Point(map(float, i.strip().split()))
     if list(map(float, i.strip().split())) in figura:
       print ('0')
